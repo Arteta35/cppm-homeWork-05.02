040c3e12a419d9f223592d4197a11e1af943589d
Artem Ershov <ershov.artem.a@gmail.com>
2023-01-30 23:52:56 +0300

    Добавлен класс прямоугольного треугольника

--- cppm-homeWork-05.02/cppm-homeWork-05.02.cpp
+++ cppm-homeWork-05.02/cppm-homeWork-05.02.cpp
@@ -4,29 +4,31 @@
 class Figure
 {
 public:
-	Figure() {};
 
-	int get_sides() {
-		name();
-		std::cout << count << std::endl;
-		return count;
+	void get_sides(int a, int b, int c) {
+		
+		this->a = a, this->b = b, this->c = c;
+
+	}
+
+	void get_angle(int A, int B, int C) {
+
+		this->A = A, this->B = B, this->C = C;
+
+	}
+	
+	void print() {
+
+		std::cout << "Стороны: a=" << a << " b=" << b << " c=" << c << std::endl;
+		std::cout << "Углы: A=" << A << " B=" << B << " C=" << C << std::endl << std::endl;
+
 	}
 
 protected:
 
 	int count = 0;
 
-	void name() {
-		if (count == 0) {
-			std::cout << "Фигура: ";
-		}
-		else if (count == 3) {
-			std::cout << "Треугольник: ";
-		}
-		else {
-			std::cout << "Четырёхугольник: ";
-		}
-	}
+	int a, b, c, A, B, C;
 
 };
 
@@ -34,11 +36,39 @@ class Triangle : public Figure
 {
 public:
 
+	Triangle(int a, int b, int c, int A, int B, int C) {
+		this->a = a, this->b = b, this->c = c, this->A = A, this->B = B, this->C = C;
+
+	}
+
+	void name() {
+		std::cout << "Треугольник:\n";
+	}
+
 	void medod() {
 
 		count = 3;
-		get_sides();
+		get_sides(a, b, c);
+		get_angle(A, B, C);
+		print();
 
+	}
+
+protected:
+
+	int a, b, c, A, B , C;
+
+};
+
+class Triangle_pr : public Triangle
+{
+public:
+
+	Triangle_pr(int a, int b, int c, int A, int B, int C) : Triangle (a,b,c,A,B,C) {
+	}
+
+	void name() {
+		std::cout << "Прямоугольный реугольник:\n";
 	}
 
 protected:
@@ -56,11 +86,15 @@ public:
 	void medod() {
 
 		count = 4;
-		get_sides();
-
+	
 	}
 
 };
+
+void print(Figure* figure) {
+	figure->print();
+}
+
 
 
 int main()
@@ -70,14 +104,16 @@ int main()
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	Triangle tri;
+	Triangle tri(10, 20, 30, 50, 60, 70);
+	Triangle_pr tri_pr(30, 20, 30, 50, 60, 70);
 	Quadrangle qu;
 
-	std::cout << "Колличество сторон:" << std::endl;
-
-	tri.get_sides();
+	tri.name();
 	tri.medod();
-	qu.medod();
+	tri_pr.name();
+	tri_pr.medod();
+
+	//qu.medod();
 
 
 }
